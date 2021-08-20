83a2901e502c23bccf9e559176d2bf33f42da099
chrishoran8 <horan_chris@hotmail.co.uk>
2021-08-20 10:13:13 +0100

    posh word eater

--- PythonEssentialsModule3/PythonEssentialsModule3.py
+++ PythonEssentialsModule3/PythonEssentialsModule3.py
@@ -94,5 +94,30 @@ for letter in user_word:
 
 
 
+#posh word eater
+
+word_without_vowels = ""
+
+# Prompt the user to enter a word
+# and assign it to the user_word variable.
+user_word = input("Enter a word")
+
+#use user_word = user_word.upper() 
+user_word = user_word.upper()
+
+for letter in user_word:
+    # Complete the body of the loop.
+    #use conditional execution and the 
+    #continue statement to "eat" the following 
+    #vowels A, E, I, O, U from the inputted word;
+    if letter in "AEIOU":
+        continue
+    
+    #assign the uneaten letters to the word_without_vowels 
+    #variable and print the variable to the screen.
+    word_without_vowels = word_without_vowels + letter
+
+# Print the word assigned to word_without_vowels.
+print(word_without_vowels)
 
     
