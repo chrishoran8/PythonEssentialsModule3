3909bac7d8cc72942c782ae5717b86209c32b53b
chrishoran8 <horan_chris@hotmail.co.uk>
2021-08-20 10:05:58 +0100

    upper

--- PythonEssentialsModule3/PythonEssentialsModule3.py
+++ PythonEssentialsModule3/PythonEssentialsModule3.py
@@ -61,4 +61,38 @@ while True:
         break
     
 print("You've successfully left the loop.")
+
+
+
+
+#word eater
+
+# Prompt the user to enter a word
+# and assign it to the user_word variable.
+#ask the user to enter a word;
+user_word = input("Enter a word please")
+
+#use user_word = user_word.upper() to convert 
+#the word entered by the user to upper case; 
+#we'll talk about the so-called string methods 
+#and the upper() method very soon - don't worry;
+user_word = user_word.upper()
+
+for letter in user_word: 
+    if letter in "AEIOU":continue
+    print(letter)
+    
+    #use conditional execution and the continue 
+    #statement to "eat" the following vowels 
+    #A, E, I, O, U from the inputted word;
+    #print the uneaten letters to the screen, 
+    #each one of them on a separate line.
+
+
+    # Complete the body of the for loop.
+
+
+
+
+
     
